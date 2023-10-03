dbb39bcc62b2439ad13cf62b1ece57cd8ada8eb8
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-10-03 15:49:49 +0100

    BatchAddProducts - Implemented and Tested

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -35,4 +35,18 @@ export class Catalogue {
     return result;
   }
 
+  batchAddProducts(batch) {
+    const invalidAdditions = batch.products.filter(
+      (product) => this.findProductById(product.id) !== undefined
+    );
+    if (invalidAdditions.length > 0 ) {
+      throw new Error('Bad Batch')
+    }
+    const validAdditions = batch.products.filter(
+      (product) => product.quantityInStock > 0
+    );
+    validAdditions.forEach( (p) => this.addProduct(p) );
+    return validAdditions.length;
+  }
+
 }
