1ce6fa8391f733acf5fa4af03328f95e86447c8c
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-10-03 15:43:25 +0100

    Implemented and tested removeProductById

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -16,4 +16,16 @@ export class Catalogue {
     }
     return false;
   }
+
+  removeProductById(id) {
+    const removedProduct = this.findProductById(id);
+    if (removedProduct) {
+      this.products = this.products.filter(
+        (product) => product.id !== id // ***********
+      );
+    }
+    return removedProduct;
+  }
+
+  
 }
--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -6,11 +6,13 @@ const expect = chai.expect;
 let cat = null;
 let batch = null;
 
-// Setup
-cat = new Catalogue("Test Catalogue");
-cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
-cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
-cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
+describe("Catalogue", () => {
+  beforeEach(() => {   // NEW CODE
+    cat = new Catalogue("Test Catalogue");
+    cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0, 10));
+    cat.addProduct(new Product("A124", "Product 2", 100, 10.0, 10));
+    cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0, 10));
+  });
 
 describe("Catalogue", () => {
   describe("findProductById", function () {
@@ -20,6 +22,20 @@ describe("Catalogue", () => {
     });
     it("should return undefined for invalid product id", function () {
       const result = cat.findProductById("A321");
+      expect(result).to.be.undefined;
+    });
+  });
+});
+describe("removeProductById", () => {
+    it("should remove product with a valid id", function () {
+      let result = cat.removeProductById("A123");
+      expect(result.id).to.equal("A123");
+      // Check object state
+      result = cat.findProductById("A123");
+      expect(result).to.be.undefined;
+    });
+    it("should return undefined when asked to remove invalid product", function () {
+      const result = cat.removeProductById("A321");
       expect(result).to.be.undefined;
     });
   });
