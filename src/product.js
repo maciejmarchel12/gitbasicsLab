325cc32530b9fc9de8c693b79f5c00a8c7388762
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-10-03 15:37:38 +0100

    initial project structure

---
+++ src/product.js
@@ -0,0 +1,9 @@
+export class Product {
+    constructor(id,name,initialQuantity, reorderLevel, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = initialQuantity
+        this.reorderLevel = reorderLevel
+    }
+}
---
+++ test/catalogue-test.js
@@ -0,0 +1,5 @@
+import chai from "chai";
+import {Catalogue} from "../src/productCatalogue.js";
+import {Product} from "../src/product.js";
+
+const expect = chai.expect;
